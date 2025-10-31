4c9be54e25a859c075d8ef25041a72385a67cd35
Mimi55-gif <emiarmas126@gmail.com>
2025-10-31 12:42:04 -0500

    Delete xor.py

--- xor.py
+++
@@ -1,8 +0,0 @@
-import random 
-def xor (text, key):
-    key = [random.randint(0,255) for _ in range(len(text))]
-    cyphertext = ""
-    for i in range (len(text)):
-        cyphertext += chr(ord(text[i])) ^ ord(key[i% (len(key))])
-        
-    return cyphertext
